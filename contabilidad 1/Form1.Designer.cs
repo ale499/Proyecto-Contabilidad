c31523bcb2008e15f6a5d40d780925f1871985d8
ale499 <gimenezalejandro083@gmail.com>
2023-11-14 17:13:44 -0300

    inicio del proyecto

---
+++ contabilidad 1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace contabilidad_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonLD = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BotonLD
+            // 
+            this.BotonLD.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BotonLD.Location = new System.Drawing.Point(73, 150);
+            this.BotonLD.Name = "BotonLD";
+            this.BotonLD.Size = new System.Drawing.Size(240, 113);
+            this.BotonLD.TabIndex = 0;
+            this.BotonLD.Text = "Libro Diario";
+            this.BotonLD.UseVisualStyleBackColor = false;
+            this.BotonLD.Click += new System.EventHandler(this.BotonLD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotonLD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button BotonLD;
+
+        #endregion
+    }
+}
